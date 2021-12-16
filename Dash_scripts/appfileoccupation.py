11d91b57bac91684292abc110fe7d06b20cc1cc1
arnaudguibbert <arnaud.guibbert@epfl.ch>
2021-12-16 22:58:06 +0100

    P-values, it seems that ADA is a kind of roulot compresseur

--- Dash_scripts/appfileoccupation.py
+++ Dash_scripts/appfileoccupation.py
@@ -28,6 +28,7 @@ def select_by_year_occupation(low,high,dash_df):
 
 # Get the datas 
 dash_data = pd.read_csv("datas/dash_data_occupation.csv.gz",compression="infer")
+P_val_df = pd.read_csv("datas/dash_datas_sector_p_values.csv",index_col=0)
 
 # Get the list of the occupations
 occupations = list(dash_data.columns[:-2])
@@ -44,6 +45,8 @@ options = [{"label": occup, "value": occup,
 
 # Define the layout of the application
 app.layout = html.Div([
+        html.Div([dcc.Graph(id="heat-map",responsive=True,style={'height': '90vh'})],
+                  style = {"width":"60%","margin-left":"20%","margin-right":"20%","margin-top":30}),
         html.Div([html.P("Year",style={"display": "flex",
                                 "font-family":"Helvetica",
                                 "font-size": 18,
@@ -58,13 +61,13 @@ app.layout = html.Div([
         html.Div([dcc.Dropdown(id='drop-down',multi=True,
                                style={"color":"black","font-family":"Helvetica","font-size": 18},
                                options=options,value=["Art","Health","Economy"])],
-                 style = {"width":"60%","margin-left":"20%","width":"60%","margin-right":"20%"}),
+                 style = {"width":"60%","margin-left":"20%","margin-right":"20%"}),
         html.Div([dcc.Graph(id="pie-chart",responsive=True)])
     ])
 
 
 # function that will update pies and drop down values 
-@app.callback(Output("pie-chart", "figure"),
+@app.callback([Output("pie-chart", "figure"),Output("heat-map", "figure")],
               [Input("range-slider","value"),Input("drop-down","value")])
 def update_pies(slider_range,values_selected):
     # get the year range wanted 
@@ -109,7 +112,23 @@ def update_pies(slider_range,values_selected):
                         "y":1.2,
                         "xanchor":"center",
                         "x":0.5})
-    return fig
+    # Heat map figure now for p-values 
+    fig_heat_sheet = px.imshow(P_val_df,range_color=[0,1],labels=dict(color="p-value"))
+    fig_heat = go.Figure()
+    for trace in fig_heat_sheet["data"]:
+        fig_heat.add_trace(trace)
+    fig_heat.update_layout(title = {'text': "Result of the Welch's t-test (p-values)",
+                            'y':0.95,
+                            'x':0.5,
+                            'xanchor': 'center',
+                            'yanchor': 'top',"font":{"family":"Helvetica","size":20}},
+                            legend={"font":{"family":"Helvetica","size":10},
+                            "orientation":"h",
+                            "yanchor":"top",
+                            "y":1.2,
+                            "xanchor":"center",
+                            "x":0.5})
+    return fig, fig_heat
 
 if __name__ == '__main__':
     app.run_server()
